17a6989add90698eaa6d59714b62711984be4ddb
Juan Pablo Chaparro <36649829+Chapzo@users.noreply.github.com>
2018-03-20 21:19:50 -0500

    CAmbios ayudaaaaaaaaaaaaa

--- Negocio/paseador.cs
+++ Negocio/paseador.cs
@@ -11,11 +11,11 @@ namespace Negocio
     {
 
         PyxisDataContext data = new PyxisDataContext();
-        public bool CrearPaseador(int id, int disponibilidad, string experiencia, string especialidad, float precio, string nombre)
+        public bool CrearPaseador(int id, int disponibilidad, string experiencia, string especialidad, float precio, string nombre, int HoraIni, int HoraFin, string Dias)
         {
             try
             {
-                data.Crearpaseador(id, disponibilidad, experiencia, especialidad, precio);
+                data.Crearpaseador(id, disponibilidad, experiencia, especialidad, precio, HoraIni, HoraFin, Dias);
                 setCodigo("ok");
                 setRTA("Muchas gracias "+nombre+", ahora eres uno de nuestros paseadores");
                 return true;
