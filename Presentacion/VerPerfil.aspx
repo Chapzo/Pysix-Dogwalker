d623933740c0e8d7da54f24e66d02b7ac873ff18
ivanovichleo <ivanovichleo@IVANOVICHLEO-PC>
2018-05-02 20:34:10 -0500

    haber pa que funcione la extraccion

---
+++ Presentacion/VerPerfil.aspx
@@ -0,0 +1,53 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/MasterPage.Master" AutoEventWireup="true" CodeBehind="VerPerfil.aspx.cs" Inherits="Presentacion.VerPerfil" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+
+    <h3>Hola</h3> 
+    <table>
+        
+         <tr>
+              <asp:Image ID="Image1" runat="server" />
+        </tr>
+        <tr>
+            <td>  <asp:Label ID="LblNom" runat="server" Text="Nombre"></asp:Label>  </td>
+            <td> <asp:TextBox ID="Txtnom" runat="server" > </asp:TextBox> </td>     
+        </tr>
+         <tr>
+            <td>  <asp:Label ID="LblApe" runat="server" Text="Apellido"></asp:Label>  </td>
+            <td> <asp:TextBox ID="TxtApe" runat="server" > </asp:TextBox> </td>     
+        </tr>
+         <tr>
+            <td>  <asp:Label ID="LblCorreo" runat="server" Text="Correo"></asp:Label>  </td>
+            <td> <asp:TextBox ID="TxtCorreo" runat="server" > </asp:TextBox> </td>     
+        </tr>
+         <tr>
+            <td>  <asp:Label ID="LblDo" runat="server" Text="Numero de documento"></asp:Label>  </td>
+            <td> <asp:TextBox ID="TxtDo" runat="server" > </asp:TextBox> </td>     
+        </tr>
+         <tr>
+            <td>  <asp:Label ID="Lbllocalidad" runat="server" Text="Localidad"></asp:Label>  </td>
+            <td> <asp:TextBox ID="TxtLocalidad" runat="server" > </asp:TextBox> </td>     
+        </tr>
+         <tr>
+            <td>  <asp:Label ID="LblTelefono" runat="server" Text="Telefono"></asp:Label>  </td>
+            <td> <asp:TextBox ID="TxtTelefono" runat="server" > </asp:TextBox> </td>     
+        </tr>
+         <tr>
+            <td>  <asp:Label ID="Lblrol" runat="server" Text="Rol"></asp:Label>  </td>
+            <td> <asp:TextBox ID="TextBox6" runat="server" > </asp:TextBox> </td>     
+        </tr>
+        
+         <tr>
+            <td>  <asp:Label ID="Label8" runat="server" Text="Nombre"></asp:Label>  </td>
+            <td> <asp:TextBox ID="TextBox8" runat="server" > </asp:TextBox> </td>     
+        </tr>
+         <tr>
+            <td>  <asp:Label ID="Label9" runat="server" Text="Nombre"></asp:Label>  </td>
+            <td> <asp:TextBox ID="TextBox9" runat="server" > </asp:TextBox> </td>     
+        </tr>
+
+    </table>
+    
+
+</asp:Content>
---
+++ Presentacion/VerPerfil.aspx.designer.cs
@@ -0,0 +1,186 @@
+﻿//------------------------------------------------------------------------------
+// <generado automáticamente>
+//     Este código fue generado por una herramienta.
+//
+//     Los cambios en este archivo podrían causar un comportamiento incorrecto y se perderán si
+//     se vuelve a generar el código. 
+// </generado automáticamente>
+//------------------------------------------------------------------------------
+
+namespace Presentacion {
+    
+    
+    public partial class VerPerfil {
+        
+        /// <summary>
+        /// Control Image1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Image Image1;
+        
+        /// <summary>
+        /// Control LblNom.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label LblNom;
+        
+        /// <summary>
+        /// Control Txtnom.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox Txtnom;
+        
+        /// <summary>
+        /// Control LblApe.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label LblApe;
+        
+        /// <summary>
+        /// Control TxtApe.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TxtApe;
+        
+        /// <summary>
+        /// Control LblCorreo.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label LblCorreo;
+        
+        /// <summary>
+        /// Control TxtCorreo.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TxtCorreo;
+        
+        /// <summary>
+        /// Control LblDo.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label LblDo;
+        
+        /// <summary>
+        /// Control TxtDo.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TxtDo;
+        
+        /// <summary>
+        /// Control Lbllocalidad.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Lbllocalidad;
+        
+        /// <summary>
+        /// Control TxtLocalidad.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TxtLocalidad;
+        
+        /// <summary>
+        /// Control LblTelefono.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label LblTelefono;
+        
+        /// <summary>
+        /// Control TxtTelefono.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TxtTelefono;
+        
+        /// <summary>
+        /// Control Lblrol.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Lblrol;
+        
+        /// <summary>
+        /// Control TextBox6.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TextBox6;
+        
+        /// <summary>
+        /// Control Label8.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label8;
+        
+        /// <summary>
+        /// Control TextBox8.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TextBox8;
+        
+        /// <summary>
+        /// Control Label9.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label9;
+        
+        /// <summary>
+        /// Control TextBox9.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TextBox9;
+    }
+}
